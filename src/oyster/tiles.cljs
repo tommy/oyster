ae1fd8ed5121f87dda7ca48a166bcc05cecea5ba
Tommy Rogers <tommysayshello@gmail.com>
2013-11-18 15:07:13 -0800

    Add tilled-soil tile and put styling in tiles.cljs

--- src/oyster/tiles.cljs
+++ src/oyster/tiles.cljs
@@ -2,12 +2,19 @@
 
 (def all-tiles
   {:water {:char \~
+           :style {:color "blue"}
            :description "Water."}
 
    :grass {:char \,
+           :style {:color "green"}
            :description "Grass."}
 
+   :tilled-soil {:char \=
+                 :style {:color "brown"}
+                 :description "Tilled soil."}
+
    :beach {:char \.
+           :style {:color "orange"}
            :description "A sandy beach."}
 
    :nothing {:char \space
